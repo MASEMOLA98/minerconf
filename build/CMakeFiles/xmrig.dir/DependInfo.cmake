59e86f86acf130a55f3b966a604a75b8532ade0f
Mark Allen Evans <president@worldvaporexpo.com>
2018-05-14 22:24:06 -0400

    added

---
+++ build/CMakeFiles/xmrig.dir/DependInfo.cmake
@@ -0,0 +1,100 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/data/data/com.termux/files/home/xmrig/src/crypto/c_blake256.c" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/crypto/c_blake256.c.o"
+  "/data/data/com.termux/files/home/xmrig/src/crypto/c_groestl.c" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/crypto/c_groestl.c.o"
+  "/data/data/com.termux/files/home/xmrig/src/crypto/c_jh.c" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/crypto/c_jh.c.o"
+  "/data/data/com.termux/files/home/xmrig/src/crypto/c_skein.c" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/crypto/c_skein.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "HAVE_SYSLOG_H"
+  "NDEBUG"
+  "UNICODE"
+  "XMRIG_ARM"
+  "XMRIG_ARMv8"
+  "XMRIG_NO_API"
+  "XMRIG_NO_HTTPD"
+  "XMRIG_NO_LIBCPUID"
+  "__STDC_FORMAT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  "../src/3rdparty"
+  "/data/data/com.termux/files/usr/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/data/data/com.termux/files/home/xmrig/src/App.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/App.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/App_unix.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/App_unix.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/Cpu_arm.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/Cpu_arm.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/Cpu_unix.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/Cpu_unix.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/Mem.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/Mem.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/Mem_unix.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/Mem_unix.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/Summary.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/Summary.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/api/NetworkState.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/api/NetworkState.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/common/Console.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/common/Console.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/common/Platform.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/common/Platform.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/common/Platform_unix.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/common/Platform_unix.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/common/config/CommonConfig.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/common/config/CommonConfig.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/common/config/ConfigLoader.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/common/config/ConfigLoader.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/common/config/ConfigWatcher.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/common/config/ConfigWatcher.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/common/crypto/Algorithm.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/common/crypto/Algorithm.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/common/crypto/keccak.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/common/crypto/keccak.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/common/log/ConsoleLog.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/common/log/ConsoleLog.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/common/log/FileLog.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/common/log/FileLog.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/common/log/Log.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/common/log/Log.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/common/log/SysLog.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/common/log/SysLog.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/common/net/Client.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/common/net/Client.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/common/net/Job.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/common/net/Job.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/common/net/Pool.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/common/net/Pool.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/common/net/SubmitResult.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/common/net/SubmitResult.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/common/net/strategies/FailoverStrategy.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/common/net/strategies/FailoverStrategy.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/common/net/strategies/SinglePoolStrategy.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/common/net/strategies/SinglePoolStrategy.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/core/Config.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/core/Config.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/core/Controller.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/core/Controller.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/net/Network.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/net/Network.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/net/strategies/DonateStrategy.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/net/strategies/DonateStrategy.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/workers/CpuThread.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/workers/CpuThread.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/workers/Handle.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/workers/Handle.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/workers/Hashrate.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/workers/Hashrate.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/workers/MultiWorker.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/workers/MultiWorker.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/workers/Worker.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/workers/Worker.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/workers/Workers.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/workers/Workers.cpp.o"
+  "/data/data/com.termux/files/home/xmrig/src/xmrig.cpp" "/data/data/com.termux/files/home/xmrig/build/CMakeFiles/xmrig.dir/src/xmrig.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "HAVE_SYSLOG_H"
+  "NDEBUG"
+  "UNICODE"
+  "XMRIG_ARM"
+  "XMRIG_ARMv8"
+  "XMRIG_NO_API"
+  "XMRIG_NO_HTTPD"
+  "XMRIG_NO_LIBCPUID"
+  "__STDC_FORMAT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../src/3rdparty"
+  "/data/data/com.termux/files/usr/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
